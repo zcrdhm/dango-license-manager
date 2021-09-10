2557b4834f493be42ea1b336f796a31077f864bf
张长瑞 <zcrdhm@126.com>
2021-09-10 11:38:27 +0800

    add first version

---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+include "common.php";
+echo "Welcome come to Dango License Manager</br>";
+?>
+<div style="position:absolute;left;0;bottom:0;">
+Copyright by Charry Zhang&copy;</br>
+Bolg:<a href='https://my.oschina.net/u/3059462' target='_blank'>https://my.oschina.net/u/3059462</a>
+</div>
